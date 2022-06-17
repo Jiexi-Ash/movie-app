86530a9cc0104e9db96a4c356fb6fae44cbecf25
Jiexi-Ash <lance.jiexi@gmail.com>
2022-06-17 14:25:54 +0200

    styled header section

--- components/Header.js
+++ components/Header.js
@@ -11,8 +11,8 @@ import {
 
 function Header() {
   return (
-    <div>
-      <div className="">
+    <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
+      <div className="flex">
         <HeaderIcon Icon={HomeIcon} title="HOME" />
         <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
         <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
@@ -21,9 +21,10 @@ function Header() {
 
       <Image
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/640px-IMDB_Logo_2016.svg.png"
-        width={150}
-        height={100}
+        width={100}
+        height={50}
         alt="logo-image"
+        className="cursor-pointer active:brightness-110"
       />
     </div>
   );
